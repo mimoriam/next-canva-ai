07194e2ddec2837d48ee5b6d683a0ec0f260ecee
Mimo <mimoriam420@gmail.com>
2024-09-13 18:26:29 +0500

    feat(app): sane defaults

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    typedRoutes: true,
+  },
+  poweredByHeader: false,
+};
 
 export default nextConfig;
