ed690f978017fdc791a566396fc83f7dc82d1e42
Mimo <mimoriam420@gmail.com>
2024-09-13 18:33:59 +0500

    fix(config): removed experimental feature (unsupported)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
-      <main>hello</main>
+      <Button>Hello Mimo!</Button>
     </div>
   );
 }
