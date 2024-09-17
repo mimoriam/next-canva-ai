c19be7a45e0a769eadc112718f3a397210d64a61
Mimo <mimoriam420@gmail.com>
2024-09-18 03:15:04 +0500

    feat(editor): Sidebar UI

--- src/app/features/editor/components/sidebar.tsx
+++ src/app/features/editor/components/sidebar.tsx
@@ -1,9 +1,33 @@
 "use client";
 
+import { SidebarItem } from "@/app/features/editor/components/sidebar-item";
+import {
+  LayoutTemplate,
+  ImageIcon,
+  Pencil,
+  Settings,
+  Shapes,
+  Sparkles,
+  Type,
+} from "lucide-react";
+
 export const Sidebar = () => {
   return (
     <aside className="flex h-full w-[100px] flex-col overflow-auto border-r bg-white">
-      Sidebar
+      <ul className="flex flex-col">
+        <SidebarItem icon={LayoutTemplate} label="Design" onClick={() => {}} />
+        <SidebarItem
+          icon={ImageIcon}
+          label="Image"
+          isActive={true}
+          onClick={() => {}}
+        />
+        <SidebarItem icon={Type} label="Text" onClick={() => {}} />
+        <SidebarItem icon={Shapes} label="Shapes" onClick={() => {}} />
+        <SidebarItem icon={Pencil} label="Draw" onClick={() => {}} />
+        <SidebarItem icon={Sparkles} label="AI" onClick={() => {}} />
+        <SidebarItem icon={Settings} label="Settings" onClick={() => {}} />
+      </ul>
     </aside>
   );
 };
