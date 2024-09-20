e03586153c18a1d19da849aca0f0b6afe600dd65
Mimo <mimoriam420@gmail.com>
2024-09-21 01:59:15 +0500

    chore(deps): Update nextjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
   return (
     <div>
-      <Button>Hello Mimo!</Button>
+      <Button onClick={() => router.push("/editor/123")}>Hello Mimo!</Button>
     </div>
   );
 }
